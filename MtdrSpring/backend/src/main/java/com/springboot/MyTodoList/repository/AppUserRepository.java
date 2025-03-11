080bbc0a0fc548a36935ddf97d582789ae86f6b4
alexeiddg <117191717+alexeiddg@users.noreply.github.com>
2025-03-11 17:54:25 -0600

    New data model base repos

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/AppUserRepository.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/AppUserRepository.java
@@ -1,7 +1,7 @@
 package com.springboot.MyTodoList.repository;
 
-
-import com.springboot.MyTodoList.model.ToDoItem;
+import com.springboot.MyTodoList.model.AppUser;
+import org.aspectj.lang.annotation.AdviceName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,5 +11,6 @@ import javax.transaction.Transactional;
 @Repository
 @Transactional
 @EnableTransactionManagement
-public interface ToDoItemRepository extends JpaRepository<ToDoItem,Integer> {
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    // impl
 }
--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/ProjectRepository.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/ProjectRepository.java
@@ -1,7 +1,6 @@
 package com.springboot.MyTodoList.repository;
 
-
-import com.springboot.MyTodoList.model.ToDoItem;
+import com.springboot.MyTodoList.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,5 +10,6 @@ import javax.transaction.Transactional;
 @Repository
 @Transactional
 @EnableTransactionManagement
-public interface ToDoItemRepository extends JpaRepository<ToDoItem,Integer> {
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+    // impl
 }

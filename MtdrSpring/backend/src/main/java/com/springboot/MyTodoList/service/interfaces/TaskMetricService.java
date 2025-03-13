dd044cd0643ac45038b97a6453e47c0421a1e61b
alexeiddg <117191717+alexeiddg@users.noreply.github.com>
2025-03-13 10:01:15 -0600

    Task Metric Serivce

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/service/interfaces/TaskMetricService.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/service/interfaces/TaskMetricService.java
@@ -10,27 +10,14 @@ public interface TaskMetricService {
     void deleteMetric(Long id);
 
     // Developer Performance
-    int getTotalAssignedTasksByDeveloper(Long developerId);
-    int getCompletedTasksByDeveloper(Long developerId);
-    Long getAvgCompletionTimeByDeveloper(Long developerId);
-    int getTotalBugsFixedByDeveloper(Long developerId);
-    int getTotalFeaturesCompletedByDeveloper(Long developerId);
     float calculateSprintCompletionRateByDeveloper(Long developerId, Long sprintId);
     float calculateEfficiencyScoreByDeveloper(Long developerId);
 
     // Team Performance
-    int getTotalAssignedTasksByTeam(Long teamId);
-    int getCompletedTasksByTeam(Long teamId);
-    Long getAvgCompletionTimeByTeam(Long teamId);
-    int getTotalBugsFixedByTeam(Long teamId);
-    int getTotalFeaturesCompletedByTeam(Long teamId);
     float calculateSprintCompletionRateByTeam(Long teamId, Long sprintId);
     float calculateEfficiencyScoreByTeam(Long teamId);
 
     // Project & Sprint Performance
-    int getTotalAssignedTasksByProject(Long projectId);
-    int getCompletedTasksByProject(Long projectId);
-    float calculateSprintVelocity(Long sprintId);
     float calculateSprintCompletionRateByProject(Long projectId);
     float calculateEfficiencyScoreByProject(Long projectId);
 }
